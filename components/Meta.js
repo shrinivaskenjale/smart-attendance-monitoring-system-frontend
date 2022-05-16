1d855dab3e75e18e0d40ceab33b8d2b8e1d1d705
Shrinivas Kenjale <sskenjale22@proton.me>
2022-05-16 12:22:49 +0530

    add landing page

--- components/Meta.js
+++ components/Meta.js
@@ -4,7 +4,7 @@ const Meta = (props) => (
     <title>{props.title}</title>
     <meta
       name="description"
-      content="Attendance tracking made simple. Smart Attendance Monitoring System changes and speeds up the attendance taking process in educational institutions."
+      content="Attendance tracking made simple. Smart Attendance Monitoring System changes and speeds up the attendance management process in educational institutions."
     />
     <meta property="og:type" content="website" />
     <meta
@@ -15,7 +15,7 @@ const Meta = (props) => (
     <meta
       name="og:description"
       property="og:description"
-      content="Attendance tracking made simple. Smart Attendance Monitoring System changes and speeds up the attendance taking process in educational institutions."
+      content="Attendance tracking made simple. Smart Attendance Monitoring System changes and speeds up the attendance management process in educational institutions."
     />
     <meta
       property="og:site_name"
@@ -29,14 +29,14 @@ const Meta = (props) => (
     <meta name="twitter:title" content="Smart Attendance Monitoring System" />
     <meta
       name="twitter:description"
-      content="Attendance tracking made simple. Smart Attendance Monitoring System changes and speeds up the attendance taking process in educational institutions."
+      content="Attendance tracking made simple. Smart Attendance Monitoring System changes and speeds up the attendance management process in educational institutions."
     />
     <meta name="twitter:site" content="Smart Attendance Monitoring System" />
     <meta name="twitter:creator" content="Smart Attendance Monitoring System" />
     <link rel="icon" type="image/png" href="/favicon.svg" />
     <link rel="apple-touch-icon" href="/favicon.svg" />
-    <meta property="og:image" content="/homepage.png" />
-    <meta name="twitter:image" content="/homepage.png" />
+    <meta property="og:image" content="/homepage.jpeg" />
+    <meta name="twitter:image" content="/homepage.jpeg" />
     <link
       rel="canonical"
       href="https://smart-attendance-monitoring.vercel.app"
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
-import Options from "../components/Options";
 import styles from "../styles/HomePage.module.css";
 import { options } from "../data/options";
 
 const optionsArr = options.admin;
 
 const HomePage = (props) => {
-  return <div className="main-content"></div>;
+  return (
+    <div className="main-content">
+      <div className={styles.banner}>
+        <div className={styles.text}>
+          <h2>
+            Attendance tracking made <span>simple</span>.
+          </h2>
+          <p>
+            Smart Attendance Monitoring System changes and speeds up the
+            attendance management process in educational institutions.
+          </p>
+        </div>
+        <img className={styles.image} src="/phone.svg" alt="phone" />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
