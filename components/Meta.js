02898d4caae692d6806ffe04ef4677ff71f6c651
Shrinivas Kenjale <sskenjale22@proton.me>
2022-05-16 15:40:20 +0530

    fix metadata

--- components/Meta.js
+++ components/Meta.js
@@ -1,30 +1,37 @@
 import Head from "next/head";
 const Meta = (props) => (
   <Head>
-    <title>{props.title}</title>
+    <title>Smart Attendance Monitoring System</title>
     <meta
       name="description"
       content="Attendance tracking made simple. Smart Attendance Monitoring System changes and speeds up the attendance management process in educational institutions."
     />
-    <meta property="og:type" content="website" />
     <meta
-      name="og:title"
+      // name="og:title"
       property="og:title"
       content="Smart Attendance Monitoring System"
     />
     <meta
-      name="og:description"
+      property="og:url"
+      content="https://smart-attendance-monitoring.vercel.app/"
+    />
+    <meta
+      // name="og:description"
       property="og:description"
       content="Attendance tracking made simple. Smart Attendance Monitoring System changes and speeds up the attendance management process in educational institutions."
     />
     <meta
+      property="og:image"
+      content="https://smart-attendance-monitoring.vercel.app/homepage.jpeg"
+    />
+
+    <meta property="og:type" content="article" />
+
+    <meta
       property="og:site_name"
       content="Smart Attendance Monitoring System"
     />
-    <meta
-      property="og:url"
-      content="https://smart-attendance-monitoring.vercel.app"
-    />
+
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:title" content="Smart Attendance Monitoring System" />
     <meta
@@ -35,8 +42,11 @@ const Meta = (props) => (
     <meta name="twitter:creator" content="Smart Attendance Monitoring System" />
     <link rel="icon" type="image/png" href="/favicon.svg" />
     <link rel="apple-touch-icon" href="/favicon.svg" />
-    <meta property="og:image" content="/homepage.jpeg" />
-    <meta name="twitter:image" content="/homepage.jpeg" />
+
+    <meta
+      name="twitter:image"
+      content="https://smart-attendance-monitoring.vercel.app/homepage.jpeg"
+    />
     <link
       rel="canonical"
       href="https://smart-attendance-monitoring.vercel.app"
@@ -44,4 +54,3 @@ const Meta = (props) => (
   </Head>
 );
 export default Meta;
-// title
